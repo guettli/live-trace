e78baf186be3342edafe799bbd2b1fed0f880bb3
Thomas Güttler <tguettler@tbz-pariv.de>
2014-05-12 12:13:53 +0200

    - test commit.

--- live_trace/django_middleware.py
+++ live_trace/django_middleware.py
@@ -4,7 +4,7 @@ DEFAULT_LIVE_TRACE_INTERVAL=0.3
 
 class LiveTraceMiddleware:
     def __init__(self):
-        u'This code gets executed once after the start of the wsgi worker process. Not for every request!'
+        u'This code gets executed once after the start of the wsgi worker process. Not for every request.'
         seconds=getattr(settings, 'LIVE_TRACE_INTERVAL', DEFAULT_LIVE_TRACE_INTERVAL)
         if not seconds:
             return
