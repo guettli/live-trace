fcf8bbae124d7f4f3e83f28be37c157cde849f19
tguettler <tguettler@tbz-pariv.de>
2016-12-08 16:12:34 +0100

    removed comment between imports.

--- live_trace/tracerusingbackgroundthread.py
+++ live_trace/tracerusingbackgroundthread.py
@@ -3,13 +3,14 @@ import atexit
 import threading
 from .tracer import Tracer
 
-# http://stackoverflow.com/questions/13193278/understand-python-threading-bug
-threading._DummyThread._Thread__stop = lambda x: True
 
 import logging
 
 logger = logging.getLogger(__name__)
 del (logging)
+
+# http://stackoverflow.com/questions/13193278/understand-python-threading-bug
+threading._DummyThread._Thread__stop = lambda x: True
 
 is_running = threading.Semaphore()
 is_running_tracer = []
