e49a1529871fbbb5922e13612547bdd3a905d1f5
Thomas Guettler <tguettler@tbz-pariv.de>
2017-07-26 15:45:04 +0200

    setup.py used compare-with-remote as guide.

--- setup.py
+++ setup.py
@@ -1,32 +1,56 @@
-import subprocess
-
 import setuptools
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-class PyTest(setuptools.Command):
-    user_options = []
+here = path.abspath(path.dirname(__file__))
 
-    def initialize_options(self):
-        pass
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys
-
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
-
-
-setuptools.setup(
+setup(
     name='live-trace',
-    version='2015.4',
-    license="http://www.apache.org/licenses/LICENSE-2.0",
-    long_description=open('README.rst').read(),
-    packages=setuptools.find_packages(),
-    install_requires=[],
 
-    cmdclass={'test': PyTest},
+    # Updated via travisd: https://travis-ci.org/guettli/live-trace
+    # See .travis.yml
+    version='2017.11.0',
+
+    description='Trace a Python application using a deamon thread.',
+    long_description=long_description,
+
+    url='https://github.com/guettli/live-trace/',
+
+    author='Thomas Guettler',
+    author_email='info.live-trace@thomas-guettler.de',
+
+    license='Apache Software License 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=setuptools.find_packages(),
 
     entry_points={
         'console_scripts': [
