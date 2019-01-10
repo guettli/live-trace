ffd280273b696dd8dd851a3108d7a4039417ec70
Bumpversion after CI <bumpversion-after-ci@example.com>
2019-01-10 13:05:51 +0000

    Bump version: 2017.19.0 -> 2017.20.0 [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     # Updated via travisd: https://travis-ci.org/guettli/live-trace
     # See .travis.yml
-    version='2017.19.0',
+    version='2017.20.0',
 
     description='Trace a Python application using a deamon thread.',
     long_description=long_description,
