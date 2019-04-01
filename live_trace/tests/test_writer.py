8e8574e12eb7cae1d357e92b0a602f171e16dcbe
David Tiede <tidenhub@gmx.de>
2019-04-01 17:20:47 +0200

    For Python 3: Remove octal prefix

--- live_trace/tests/test_writer.py
+++ live_trace/tests/test_writer.py
@@ -12,6 +12,6 @@ from live_trace.writer import WriterToLogTemplate
 class Test(unittest.TestCase):
     def test_writer_to_log_template(self):
         writer = WriterToLogTemplate(main.outfile)
-        now = datetime.datetime(2017, 07, 31, 15, 34)
+        now = datetime.datetime(2017, 7, 31, 15, 34)
         time_part, pid_part = os.path.basename(writer.get_outfile(now=now)).split('--')
         self.assertEqual('2017-07-31-15-34-00', time_part)
